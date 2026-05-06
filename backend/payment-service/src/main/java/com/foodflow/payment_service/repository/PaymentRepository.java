f84f91e07681e61e55e01c7d64bd02975f3b5edb
Tijana <tijana@192.168.1.19>
2026-05-06 21:05:31 +0200

    Add payment GET endpoints

--- backend/payment-service/src/main/java/com/foodflow/payment_service/repository/PaymentRepository.java
+++ backend/payment-service/src/main/java/com/foodflow/payment_service/repository/PaymentRepository.java
@@ -3,5 +3,8 @@ package com.foodflow.payment_service.repository;
 import com.foodflow.payment_service.model.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    List<Payment> findByOrderId(Long orderId);
 }
